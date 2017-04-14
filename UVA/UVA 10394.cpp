c79c3fb084df3c6caf8dde1973ec374d8dc781a6
Mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2017-04-14 10:09:48 +0200

    Add files via upload

---
+++ UVA/UVA 10394.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+bool pr[(int) 2e7+5];
+int main()
+{
+    memset(pr,1,sizeof(pr));
+    for(ll i = 2 ; i <= 2e7 ; i++)
+        if(pr[i])
+            for(ll j = i*i ; j <= 2e7 ; j+=i) pr[j]= 0;
+    vector<pair<int,int> > ans ;
+    for(int i = 2 ; i < 2e7 ; i++)
+    {
+        if(pr[i] && pr[i+2]) ans.push_back({i,i+2});
+    }
+    int n ;
+    while(cin >> n)
+    {
+        cout << "(" << ans[n-1].first << ", " << ans[n-1].second << ")\n" ;
+    }
+}
