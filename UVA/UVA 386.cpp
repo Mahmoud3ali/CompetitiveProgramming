7c22a95956a3907fe6d45bae87deb9595e8fd023
Mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2017-04-14 09:47:04 +0200

    Add files via upload

---
+++ UVA/UVA 386.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+int so[(int) 1e7+5] ;
+int main()
+{
+    for(int i = 1; i <= 200 ; i++) so[i*i*i] = i;
+    for(int a = 0 ; a <= 200 ; a++)
+    {
+        int y = a*a*a;
+        for(int i = 2 ; i <= 200 ; i++)
+        {
+            for(int j = i+1 ; j <= 200 ; j++)
+            {
+                int x = y-(i*i*i + j*j*j);
+                if(x > 0 && so[x] > j)
+                {
+                    cout << "Cube = " << a << ", Triple = (" << i << "," << j << "," << so[x] << ")\n" ;
+                }
+            }
+        }
+    }
+}
