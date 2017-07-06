9dd2d05ed3f17373c3ba8db9da55ef8244301206
Mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2017-07-06 10:34:28 +0200

    Add files via upload

---
+++ Timus/Timus 1178.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    int n ;
+    cin >> n ;
+    vector< pair<pair<int,int>,int> > v;
+    for(int i = 0 ; i < n ; i++)
+    {
+        int x , y;
+        cin >> x >> y;
+        v.push_back({{x,y},i+1});
+    }
+    sort(v.begin(),v.end());
+    for(int i = 0 ; i < v.size() ; i+=2)
+    {
+        cout << v[i].second << " "<< v[i+1].second << endl;
+    }
+}
---
+++ Timus/Timus 1820.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    int n , k;
+    while(cin >> n >> k)
+    {
+        double x = n*2;
+        cout << max(2,(int)ceil(x/k)) << endl;
+    }
+}
