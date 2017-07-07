34ec5ba30eacb98275219b053fa956e999cc294c
Mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2017-07-07 18:29:12 +0200

    Update LIVEARCHIVE 6195.cpp

--- LiveArchive/LIVEARCHIVE 6195.cpp
+++ LiveArchive/LIVEARCHIVE 6195.cpp
@@ -1,54 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-bool ans, vis[30][30];
-string s, best;
-int n, m ;
-void go(int i, int j, int cnt)
-{
-    if(i < 1 || j < 1 || i > n || j > m || ans) return ;
-    if(vis[i][j]) return ;
-    vis[i][j] = 1 ;
-    s+= ('A'-1+j);
-    s+= ('0'+i);
-    //cout << i << " " << j << " " << cnt+1 << endl;
-    if(cnt+1 == n*m)
-    {
-        best = s;
-        ans = 1;
-        return ;
-    }
-    go(i-1,j-2,cnt+1);
-    go(i+1,j-2,cnt+1);
-    go(i-2,j-1,cnt+1);
-    go(i+2,j-1,cnt+1);
-    go(i-2,j+1,cnt+1);
-    go(i+2,j+1,cnt+1);
-    go(i-1,j+2,cnt+1);
-    go(i+1,j+2,cnt+1);
-    s.pop_back();
-    s.pop_back();
-    vis[i][j] = 0;
-}
 int main()
 {
-    int t;
-    cin >> t;
-    while(t--)
+    int n, m;
+    while(cin >> n >> m && n+m != 0)
     {
-        ans = 0;
-        memset(vis,0,sizeof(vis));
-        cin >> n >> m ;
-        for(int j = 1 ; j <= m && ! ans; j++)
+        vector< vector<int> > adj;
+        adj.resize(n+5);
+        int c[n+5];
+        memset(c,0,sizeof(c));
+        for(int i = 0 ; i < m ; i++)
         {
-            for(int i = 1 ; i <= n && !ans ; i++)
+            int x, y;
+            cin >> x >> y;
+            c[y]++;
+            adj[x].push_back(y);
+        }
+        queue<int> q;
+        for(int i = 1; i <= n ; i++) if(c[i] == 0) q.push(i);
+        int ans = 0 ;
+        bool good = 1;
+        while(q.size())
+        {
+            int f = q.size();
+            ans = max(ans,min(f,2));
+            for(int i = 0 ; i < f ; i++)
             {
-                s.clear();
-                go(i,j,0);
+                int u = q.front();
+                q.pop();
+                for(int j = 0 ; j < adj[u].size() ; j++)
+                {
+                    c[adj[u][j]]--;
+                    if(c[adj[u][j]] == 0) q.push(adj[u][j]);
+                }
             }
         }
-        if(ans)
-            cout << best << "\n";
-        else cout << "-1\n";
+        for(int i = 1 ; i <= n ; i++) if(c[i]) good = 0;
+        if(good) cout << ans << endl;
+        else cout << 0 << endl;
     }
 }
