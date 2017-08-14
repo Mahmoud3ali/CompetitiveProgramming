01547bd29c2de7d7c1dedd41c94d11d0648454d3
Mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2017-08-14 20:09:40 +0200

    Add files via upload

---
+++ PKU/PKU 3158.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    while(a.size() < 300) a+='0';
+    while(b.size() < 300) b+='0';
+    int ans = 1e9;
+    for(int k = 0 ; k < 150 ; k++)
+    {
+        for(int i = 0 , j = k;; i++, j++)
+        {
+            if(a[i] == b[j] && a[i] == '2') break;
+            if(a[i] == '0' && b[j] == '0')
+            {
+                ans = min(ans,max(j,i));
+                break;
+            }
+        }
+        for(int i = k , j = 0;; i++, j++)
+        {
+            if(a[i] == b[j] && a[i] == '2') break;
+            if(a[i] == '0' && b[j] == '0')
+            {
+                ans = min(ans,max(j,i));
+                break;
+            }
+        }
+    }
+    cout << ans << endl;
+}
