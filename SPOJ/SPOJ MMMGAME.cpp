7a58fb346378b766ecd5a038ea4347b6151a99db
Mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2017-08-24 04:26:40 +0200

    Update SPOJ MMMGAME.cpp

--- SPOJ/SPOJ MMMGAME.cpp
+++ SPOJ/SPOJ MMMGAME.cpp
@@ -3,20 +3,35 @@ using namespace std;
 typedef long long ll;
 int main()
 {
+	/*
+	 * 0 -> 1
+	 * 1 -> 0
+	 * 2 -> 2
+	 * 3 -> 3
+	 * 4 -> 4
+	 * only one special case if all are ones
+	 * that's because 1 = 0 so it's not counted as a move
+	 * so you need to make it a move yourself by counting it seperately.
+	 */
 	int t;
 	cin >> t;
 	while(t--)
 	{
-		int n, ans = 0;
+		int n, ans = 0, cnt= 0;
 		cin >> n;
 		for(int i = 0 ; i < n ; i++)
 		{
 			int x;
 			cin >> x;
-			x--;
 			ans^=x;
+			cnt+=(x==1);
 		}
-		if(ans) cout << "John\n";
+		if(cnt == n)
+		{
+			if(cnt%2 == 0) cout << "John\n";
+			else cout << "Brother\n";
+		}
+		else if(ans) cout << "John\n";
 		else cout << "Brother\n";
 	}
 }
